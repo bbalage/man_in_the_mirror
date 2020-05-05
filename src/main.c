8ecf67422215d0f93820ee8127af9e1d94e31c60
Bolyki Balázs <bbalage0@gmail.com>
2020-05-05 20:48:20 +0200

    Now passive mouse motion is handled too (same way as active).

--- src/main.c
+++ src/main.c
@@ -14,6 +14,7 @@ void set_callbacks()
     glutReshapeFunc(reshape);
     glutMouseFunc(mouse);
     glutMotionFunc(motion);
+	glutPassiveMotionFunc(motion);
     glutKeyboardFunc(keyboard);
     glutKeyboardUpFunc(keyboard_up);
     glutIdleFunc(idle);
